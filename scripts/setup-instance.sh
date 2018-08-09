366d783712bcded33308a9ce0974570260ece834
Benjamin Teuber <benjamin.teuber@freiheit.com>
2018-08-09 21:38:58 +0200

    Script fixes

--- scripts/setup-instance.sh
+++ scripts/setup-instance.sh
@@ -6,7 +6,7 @@ set -eu
 source ./config.sh
 
 ./scripts/start-instance.sh
-gcloud compute ssh "$INSTANCE_NAME" --zone "$ZONE" --command "sudo mkdir -p /leela && sudo chmod o+rw /leela"
-gcloud compute scp --zone "$ZONE" remote/*.sh "$INSTANCE_NAME":/leela/
+gcloud compute ssh "$INSTANCE_NAME" --zone "$ZONE" --command "sudo mkdir -p /leela && sudo chmod -R o+rw /leela"
+gcloud compute scp --zone "$ZONE" remote/*.sh "$INSTANCE_NAME":/leela
 gcloud compute ssh "$INSTANCE_NAME" --zone "$ZONE" --command "/leela/setup.sh"
 gcloud compute ssh "$INSTANCE_NAME" --zone "$ZONE" --command "cd /leela/ && ./leelaz -w $NETWORK-network.gz -t $CPU_COUNT --tune-only"
