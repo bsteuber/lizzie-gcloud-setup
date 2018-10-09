8ec69d88e9a1bfbb84a7fb3d224a352f0c27b595
Benjamin Teuber <benjamin.teuber@freiheit.com>
2018-10-09 11:33:58 +0200

    Extra tuner script

--- scripts/setup-instance.sh
+++ scripts/setup-instance.sh
@@ -9,4 +9,4 @@ source ./config.sh
 gcloud compute ssh "$INSTANCE_NAME" --zone "$ZONE" --command "sudo mkdir -p /leela && sudo chmod -R o+rw /leela"
 gcloud compute scp --zone "$ZONE" remote/*.sh "$INSTANCE_NAME":/leela
 gcloud compute ssh "$INSTANCE_NAME" --zone "$ZONE" --command "/leela/setup.sh"
-gcloud compute ssh "$INSTANCE_NAME" --zone "$ZONE" --command "cd /leela/ && ./leelaz -w $NETWORK-network.gz -t $CPU_COUNT --tune-only"
+./scripts/tune-instance.sh
---
+++ scripts/tune-instance.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Installs leela-zero for Lizzie on a gcloud compute instance
+set -eu
+
+source ./config.sh
+
+./scripts/start-instance.sh
+gcloud compute ssh "$INSTANCE_NAME" --zone "$ZONE" --command "cd /leela/ && ./leelaz -w $NETWORK-network.gz -t $CPU_COUNT --tune-only"
