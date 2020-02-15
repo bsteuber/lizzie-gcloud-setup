0f6af9d6bba58a4eb7192b232a207efc92df12f9
rimathia <rimathia@users.noreply.github.com>
2020-02-15 23:40:14 +0100

    corrected download of v1.3 katago networks

--- remote/download-katago-network.sh
+++ remote/download-katago-network.sh
@@ -4,8 +4,6 @@
 #gunzip g104-b20c256-s447913472-d241840887/model.txt.gz
 #cp g104-b20c256-s447913472-d241840887/model.txt .
 
-curl -L https://github.com/lightvector/KataGo/releases/download/v1.3.2/g170-b20c256x2-s1913382912-d435450331.txt.gz -o g170b20.zip
-unzip g170b20.zip
+curl -L https://github.com/lightvector/KataGo/releases/download/v1.3.2/g170-b20c256x2-s1913382912-d435450331.txt.gz -o g170b20.txt.gz
 
-curl -L https://github.com/lightvector/KataGo/releases/download/v1.3.2/g170e-b15c192-s1672170752-d466197061.txt.gz -o g170b15.zip
-unzip g170b15.zip
+curl -L https://github.com/lightvector/KataGo/releases/download/v1.3.2/g170e-b15c192-s1672170752-d466197061.txt.gz -o g170b15.txt.gz
